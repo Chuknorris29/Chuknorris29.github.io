38d784ad26387c49eb577ff4468dd8be040ce8c7
CMikolyski <35354078+Chuknorris29@users.noreply.github.com>
2018-03-09 15:23:36 -0600

    Back to GET
    Changed back to Get.

--- assignments/lesson-9/scripts/dynamic-info.js
+++ assignments/lesson-9/scripts/dynamic-info.js
@@ -1,7 +1,7 @@
 var section = document.querySelector("section");
 var requestURL = "https:byui-cit230.github.io/weather/data/towndata.json";
 var request = new XMLHttpRequest();
-request.open("POST", requestURL);
+request.open("GET", requestURL);
 request.responseType = "json";
 request.send();
 request.onload = function() {
