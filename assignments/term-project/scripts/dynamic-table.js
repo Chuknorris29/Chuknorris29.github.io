28251f275920fd0e77e63dc1f32716011c580ded
CMikolyski <35354078+Chuknorris29@users.noreply.github.com>
2018-04-12 11:58:03 -0500

    Term-Project 2.9
    Almost Done.

--- assignments/term-project/scripts/dynamic-table.js
+++ assignments/term-project/scripts/dynamic-table.js
@@ -1,5 +1,5 @@
 var tableBody = document.querySelector("tbody");
-var requestURL = "data/service-data.json";
+var requestURL = "https://chuknorris29.github.io/assignments/term-project/data/service-data.json";
 var request = new XMLHttpRequest();
 request.open("GET", requestURL);
 request.responseType = "json";
